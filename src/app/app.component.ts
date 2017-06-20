85d4662d53d28cc3e8c31fda56c13cba8f6a8da8
Søren Reinke <soren@reinke.dk>
2017-06-20 11:55:58 +0200

    exercise 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
-  template:'<md-toolbar color="primary">Contacts</md-toolbar>',
-//  templateUrl: './app.component.html',
+  template: `
+  <md-toolbar color="primary">Contacts</md-toolbar>
+  <md-list>
+    <md-list-item>
+      <img md-list-avatar [src]="contact.image" alt="Picture of {{contact.name}}">
+      <h3 md-line>{{contact.name}}</h3>
+    </md-list-item>
+  </md-list>`,
   styleUrls: ['./app.component.scss']
 })
 export class ContactsAppComponent {
   title = 'Angular 2 Master Class setup works!';
+
+  contact: Contact = {
+    id: 6,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  }
 }
