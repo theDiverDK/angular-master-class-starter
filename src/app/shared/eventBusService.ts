39293c8577645028d60daecd8a9efbe48d38a39a
Søren Reinke <soren@reinke.dk>
2017-06-22 09:41:11 +0200

    cleanup

--- src/app/shared/eventBusService.ts
+++ src/app/shared/eventBusService.ts
@@ -3,23 +3,26 @@ import 'rxjs/add/operator/filter'
 import 'rxjs/add/operator/map'
 import { Injectable } from '@angular/core';
 
-export interface EventBusArgs{
+export interface EventBusArgs {
   type: string;
   data: any;
 }
 
 @Injectable()
-export class EventBusService  {
+export class EventBusService {
 
   private _messages$ = new Subject<EventBusArgs>();
 
   emit(eventType: string, data: any) {
-    this._messages$.next({ type: eventType, data: data });
+    this._messages$.next({
+      type: eventType,
+      data: data
+    });
   }
 
   observe(eventType: string) {
     return this._messages$
-               .filter(args => args.type === eventType)
-               .map(args => args.data);
+      .filter(args => args.type === eventType)
+      .map(args => args.data);
   }
 }
